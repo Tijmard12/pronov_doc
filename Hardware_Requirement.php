f482bb054f957692c49592e78e2f0523d52b3e36
Highbee4u <Highbee4u@gmail.com>
2017-09-11 11:59:24 +0100

    second commit

---
+++ Hardware_Requirement.php
@@ -0,0 +1,28 @@
+<?php
+require("Header.php");
+?>
+
+<div style="padding:20px; height:20px; background-color:rgb(255, 253, 253)">
+            <div id="title">
+                <h1 id="Heading">Hardware Requirement</h1>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div style="padding:20px; height:20px; background-color:rgb(255, 253, 253); left:305px; padding-left:320px;font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif text-margin: 10px">
+    Any system to be used must preferably have the following configuration:<br><br>
+<ul>
+    <li>A colour monitor with a minimum resolution of 1280 X 800 pixels, preferably an LCD screen</li>
+    <li>A 3GHz processor and above</li>
+    <li>A 3GB memory (RAM) or above.</li>
+    <li>A minimum of 256GB hard disk space.</li>
+    <li>A PS/2 port keyboard or a compatible</li>
+    <li>A Microsoft mouse or any compatible</li>
+ </ul>
+
+     </div>
+
+    </div>
+</body>
+
+</html>
