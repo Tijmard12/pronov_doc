1772b36025da747a2b49d2744344731691c9aa6f
octacore19 <chuksgid@gmail.com>
2017-10-02 11:44:31 +0100

    commit 5

--- js/main.js
+++ js/main.js
@@ -5,7 +5,28 @@ function changedColor(x){
 function normalColor(x){
     x.style.color = "white";
 }
+
 function load_config() {
      document.getElementById("content").innerHTML=
-     '<iframe src="add_configuration.html" style="width:980px; min-height:480px; border:none;" ></iframe>';
+     '<iframe src="add_config.html" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_config1() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="add_config.html#manage" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_config2() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="add_config.html#search" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_groups() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="add_groups.html" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_groups1() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="add_groups.html#manage" style="width:980px; min-height:480px; border:none;" ></iframe>';
 }
