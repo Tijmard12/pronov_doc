ba1f33521155cb70a441aee83c6832ea137ebbd1
octacore19 <chuksgid@gmail.com>
2017-10-02 07:43:09 +0100

    Commits 4

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function changedColor(x){
+    x.style.color = "silver";
+}
+
+function normalColor(x){
+    x.style.color = "white";
+}
+function load_config() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="add_configuration.html" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
