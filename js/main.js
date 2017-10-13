e1c99d0e77db7c21d263987569fceb7faec8c1a5
Mubarak01 <mubaracksulaymann@gmail.com>
2017-10-13 13:27:06 +0100

    New commit - updated with changes

---
+++ js/main.js
@@ -0,0 +1,116 @@
+function changedColor(x){
+    x.style.color = "silver";
+}
+
+function normalColor(x){
+    x.style.color = "white";
+}
+
+function load_system_req() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="system_requirement.html" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_config() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="config.html" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_config1() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="config.html#manage" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_config2() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="config.html#search" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_branches() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="branches.html#add" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_branches1() {
+     document.getElementById("content").innerHTML=
+     '<iframe src="branches.html#access" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_branches2() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="branches.html#manage" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_users() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="users.html#add" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_users1() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="users.html#manage" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_registration() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="access.html#registration" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_step1() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="access.html#step1" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_step2() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="access.html#step2" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_step3() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="access.html#step3" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_step4() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="access.html#step4" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_login() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="access.html#login" style="width:980px; min-height:480px; border:none;" ></iframe>';
+}
+
+function load_inventory0() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="inventory.html#add" style="width:980px; min-height:480px; border:none" ></iframe>';
+}
+
+function load_inventory() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="inventory.html#barcode" style="width:980px; min-height:480px; border:none" ></iframe>';
+}
+
+function load_inventory1() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="inventory.html#nobarcode" style="width:980px; min-height:480px; border:none" ></iframe>';
+}
+function load_inventory2() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="inventory.html#search" style="width:980px; min-height:480px; border:none" ></iframe>';
+}
+
+function load_inventory3() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="inventory.html#shelve" style="width:980px; min-height:480px; border:none" ></iframe>';
+}
+
+function load_inventory4() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="inventory.html#modify" style="width:980px; min-height:480px; border:none" ></iframe>';
+}
+
+function load_inventory5() {
+    document.getElementById("content").innerHTML=
+    '<iframe src="inventory.html#modify-" style="width:980px; min-height:480px; border:none" ></iframe>';
+}
