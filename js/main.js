03ecf5c4c8ebc1c9c9095a997e035e6a211cad66
Giwa Ahmad Tijani <tijmard@gmail.com>
2017-10-27 17:46:11 +0100

    most Updated version

--- js/main.js
+++ js/main.js
@@ -16,202 +16,118 @@ function infoDisplay()
 function load_system_req() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="system_requirement.html" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="system_requirement.html"></iframe>';
->>>>>>> master
 }
 
 function load_config() 
 {
      document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-     '<iframe src="config.html" style="width:980px; min-height:520px; border:none;" ></iframe>';     
-=======
      '<iframe src="config.html"></iframe>';     
-}
-
-function load_config1() 
-{
-     document.getElementById("content").innerHTML=
-     '<iframe src="config.html#manage"" ></iframe>';
-}
-
-function load_config2() 
-{
-     document.getElementById("content").innerHTML=
-     '<iframe src="config.html#search"></iframe>';
->>>>>>> master
 }
 
 function load_branches() 
 {
      document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-     '<iframe src="branches.html#add" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
      '<iframe src="branches.html#add"></iframe>';
->>>>>>> master
 }
 
 function load_branches1() 
 {
      document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-     '<iframe src="branches.html#access" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
      '<iframe src="branches.html#access"></iframe>';
->>>>>>> master
 }
 
 function load_branches2() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="branches.html#manage" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="branches.html#manage"></iframe>';
->>>>>>> master
 }
 
 function load_users() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="users.html#add" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="users.html#add"></iframe>';
->>>>>>> master
 }
 
 function load_users1() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="users.html#manage" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="users.html#manage"></iframe>';
->>>>>>> master
 }
 
 function load_step1() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="access.html#step1" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="access.html#step1"></iframe>';
->>>>>>> master
 }
 
 function load_step2() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="access.html#step2" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="access.html#step2"></iframe>';
->>>>>>> master
 }
 
 function load_step3() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="access.html#step3" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="access.html#step3"></iframe>';
->>>>>>> master
 }
 
 function load_step4() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="access.html#step4" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="access.html#step4"></iframe>';
->>>>>>> master
 }
 
 function load_login() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="access.html#login" style="width: 100%; min-height: 520px; border:none;" ></iframe>';
-=======
     '<iframe src="access.html#login"></iframe>';
->>>>>>> master
 }
 
 function load_inventory() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="inventory.html#barcode" style="width: 100%; min-height: 520px; border:none" ></iframe>';
-=======
     '<iframe src="inventory.html#barcode"></iframe>';
->>>>>>> master
 }
 
 function load_inventory1() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="inventory.html#nobarcode" style="width: 100%; min-height: 520px; border:none" ></iframe>';
-=======
     '<iframe src="inventory.html#nobarcode"></iframe>';
->>>>>>> master
 }
 function load_inventory2() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="inventory.html#search" style="width: 100%; min-height: 520px; border:none" ></iframe>';
-=======
     '<iframe src="inventory.html#search"></iframe>';
->>>>>>> master
 }
 
 function load_inventory3() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="inventory.html#shelve" style="width: 100%; min-height: 520px; border:none" ></iframe>';
-=======
     '<iframe src="inventory.html#shelve"></iframe>';
->>>>>>> master
 }
 
 function load_inventory4() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="inventory.html#modify" style="width:980px; min-height: 520px; border:none" ></iframe>';
-=======
     '<iframe src="inventory.html#modify"></iframe>';
->>>>>>> master
 }
 
 function load_inventory5() 
 {
     document.getElementById("content").innerHTML=
-<<<<<<< HEAD
-    '<iframe src="inventory.html#modify-" style="width:980px; min-height: 520px; border:none" ></iframe>';
+    '<iframe src="inventory.html#modify-"></iframe>';
 }
 
 function load_home() {
     document.getElementById("content").innerHTML=
-    '<iframe src="home.html#sale" style="width:980px; min-height: 520px; border:none" ></iframe>';
+    '<iframe src="home.html#sale"></iframe>';
 }
 
 function load_home1() {
     document.getElementById("content").innerHTML=
-    '<iframe src="home.html#query" style="width:980px; min-height: 520px; border:none" ></iframe>';
-=======
-    '<iframe src="inventory.html#modify-"></iframe>';
->>>>>>> master
+    '<iframe src="home.html#query"></iframe>';
 }
 
 
